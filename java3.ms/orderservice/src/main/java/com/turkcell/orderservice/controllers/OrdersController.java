d983b87347ebd8e42e3ba34aafa7894a6108b83c
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-04-20 13:41:37 +0300

    20.04.2024,13:41

--- java3.ms/orderservice/src/main/java/com/turkcell/orderservice/controllers/OrdersController.java
+++ java3.ms/orderservice/src/main/java/com/turkcell/orderservice/controllers/OrdersController.java
@@ -1,9 +1,11 @@
 package com.turkcell.orderservice.controllers;
 
 
+import com.turkcell.orderservice.clients.ProductServiceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,23 +14,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class OrdersController {
 
-    private final WebClient.Builder webClientBuilder;
-
+   // private final WebClient.Builder webClientBuilder;
+      private final ProductServiceClient productServiceClient;
 
     @PostMapping
-    public String addOrder(){
+    public String addOrder(@RequestParam int productId){
 
         //should go to the product service and get stock information of the products
 
-        var result= webClientBuilder
+       /* var result= webClientBuilder
                 .build()
                 .get()
-                .uri("http://localhost:8086/api/v1/products?productId=2")
+                .uri("http://localhost:8086/api/v1/products?productId="+productId)
                 .retrieve()
                 .bodyToMono(Integer.class)
                 .block();
-        
 
+*/
+
+        int stockResult=productServiceClient.getStockByProductId(productId);
         return "Order added.";
     }
 }
