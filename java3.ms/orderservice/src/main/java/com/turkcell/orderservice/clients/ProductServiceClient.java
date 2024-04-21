6b62eb1d2083aaf4697489f3690581c383b457bc
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-04-21 15:30:33 +0300

    21.04.2024,15:30

--- java3.ms/orderservice/src/main/java/com/turkcell/orderservice/clients/ProductServiceClient.java
+++ java3.ms/orderservice/src/main/java/com/turkcell/orderservice/clients/ProductServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="productservice",url="http://localhost:8086")
+@FeignClient(name="productservice")
 public interface ProductServiceClient {
     @GetMapping("/api/v1/products")
     int getStockByProductId(@RequestParam int productId);
